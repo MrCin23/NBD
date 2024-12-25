0ceb5ae6511953041eaa075a8709f07c77cf27cd
Piotr Sokolinski <piotrolinek@gmail.com>
2024-12-25 19:37:40 +0100

    containers work, repo todo

--- ORM/VMRental/src/main/java/org/example/repository/ClientRepository.java
+++ ORM/VMRental/src/main/java/org/example/repository/ClientRepository.java
@@ -8,132 +8,32 @@ import java.util.Map;
 
 public class ClientRepository {
     List<Client> clients;
-//    private final String url = "jdbc:postgresql://localhost:5432/vmrental";
-//    private final String user = "postgres";
-//    private final String password = "password";
-
 
     public ClientRepository() {
         clients = new ArrayList<Client>();
     }
 
-    //-------------METHODS---------------------------------------
-    //TODO dorobić metody z diagramu
-
     public void update(long id, Map<String, Object> fieldsToUpdate) {
-//        // Check if there are fields to update
-//        if (fieldsToUpdate == null || fieldsToUpdate.isEmpty()) {
-//            throw new IllegalArgumentException("No fields to update.");
-//        }
-//        try (Session session = sessionFactory.openSession()) {
-//            // Start the transaction
-//            Transaction transaction = session.beginTransaction();
-//
-//            // Retrieve the entity by its ID
-//            Object entity = session.get(Client.class, id);
-//            if (entity == null) {
-//                throw new IllegalArgumentException("Entity not found with id: " + id);
-//            }
-//
-//            // Update the fields dynamically (use reflection)
-//            for (Map.Entry<String, Object> entry : fieldsToUpdate.entrySet()) {
-//                String fieldName = entry.getKey();
-//                Object fieldValue = entry.getValue();
-//
-//                // Use reflection to set the field value on the entity
-//                try {
-//                    Field field = Client.class.getDeclaredField(fieldName);
-//                    field.setAccessible(true);
-//                    field.set(entity, fieldValue);
-//                } catch (NoSuchFieldException | IllegalAccessException e) {
-//                    throw new RuntimeException("Error updating field: " + fieldName, e);
-//                }
-//            }
-//
-//            // Save or update the entity
-//            session.update(entity);
-//
-//            // Commit the transaction
-//            transaction.commit();
-//            System.out.println("Entity updated successfully.");
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        throw new RuntimeException("Not implemented yet");
     }
 
     public void add(Client client) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//
-//            ClientType clientType = session.createQuery("FROM ClientType ct WHERE ct.name = :name", ClientType.class)
-//                    .setParameter("name", client.getClientType().getClass().getSimpleName())
-//                    .uniqueResult();
-//
-//            if(clientType == null) {
-//                session.save(client.getClientType());
-//            }
-//            else {
-//                client.setClientType(clientType);
-//            }
-//            session.save(client);
-//            transaction.commit();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        throw new RuntimeException("Not implemented yet");
     }
 
     public void remove(Client client) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//            session.delete(client);
-//            transaction.commit();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        clients.remove(client);
+        throw new RuntimeException("Not implemented yet");
     }
 
     public long size() {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//            Long count = (Long) session.createQuery("SELECT COUNT(c) FROM Client c").uniqueResult();
-//            transaction.commit();
-//            return count;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-        return 0;
+        throw new RuntimeException("Not implemented yet");
     }
 
     public List<Client> getClients() {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//
-//            clients = session.createQuery("FROM Client", Client.class).getResultList();
-//
-//            transaction.commit();
-//            return clients;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-        return null;
+        throw new RuntimeException("Not implemented yet");
     }
 
     public Client getClientByID(long ID) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//
-//            Client client = session.createQuery("FROM Client c WHERE c.clientID = :clientID", Client.class)
-//                    .setParameter("clientID", ID)
-//                    .uniqueResult();
-//
-//            transaction.commit();
-//            return client;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-        return null;
+        throw new RuntimeException("Not implemented yet");
     }
-
-
 }
--- ORM/VMRental/src/main/java/org/example/repository/VMachineRepository.java
+++ ORM/VMRental/src/main/java/org/example/repository/VMachineRepository.java
@@ -12,119 +12,29 @@ public class VMachineRepository {
 
     public VMachineRepository() {
         vMachines = new ArrayList<VMachine>();
-//        sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
     }
 
-    //-------------METHODS---------------------------------------
-    //TODO dorobić metody z diagramu
-
     public void update(long id, Map<String, Object> fieldsToUpdate) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//
-//            VMachine vMachine = session.createQuery("FROM VMachine vm WHERE vm.vMachineID = :vMachineID", VMachine.class)
-//                    .setParameter("vMachineID", id)
-//                    .uniqueResult();
-//
-//            for (Map.Entry<String, Object> entry : fieldsToUpdate.entrySet()) {
-//                String fieldName = entry.getKey();
-//                Object fieldValuee = entry.getValue();
-//                try {
-//                    Field field = VMachine.class.getDeclaredField(fieldName);
-//                    field.setAccessible(true);
-//                    Class<?> type = field.getType();
-//                    String fieldValue = (String)fieldValuee;
-//                    if (type == String.class) {
-//                        field.set(vMachine, fieldValue);
-//                    }
-//                    else if (type == int.class || type == Integer.class) {
-//                        field.set(vMachine, Integer.parseInt(fieldValue));
-//                    }
-//                    else if (type == boolean.class || type == Boolean.class) {
-//                        field.set(vMachine, Boolean.parseBoolean(fieldValue));
-//                    }
-//                    else if (type == long.class || type == Long.class) {
-//                        field.set(vMachine, Long.parseLong(fieldValue));
-//                    }
-//                    else if (type == double.class || type == Double.class) {
-//                        field.set(vMachine, Double.parseDouble(fieldValue));
-//                    }
-//                    else if (type == float.class || type == Float.class) {
-//                        field.set(vMachine, Float.parseFloat(fieldValue));
-//                    }
-//                    else {
-//                        field.set(vMachine, type.cast(fieldValue));
-//                    }
-//                } catch (NoSuchFieldException | IllegalAccessException e) {
-//                    throw new RuntimeException("Error updating field: " + fieldName, e);
-//                }
-//            }
-//            transaction.commit();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        throw new RuntimeException("Not implemented yet");
     }
 
     public void add(VMachine vMachine) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//            session.save(vMachine);
-//            transaction.commit();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        throw new RuntimeException("Not implemented yet");
     }
 
     public void remove(VMachine vMachine) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//            session.delete(vMachine);
-//            transaction.commit();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        vMachines.remove(vMachine);
+        throw new RuntimeException("Not implemented yet");
     }
 
     public long size() {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//            Long count = (Long) session.createQuery("SELECT COUNT(vm) FROM VMachine vm").uniqueResult();
-//            transaction.commit();
-//            return count;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-        return 0;
+        throw new RuntimeException("Not implemented yet");
     }
 
     public List<VMachine> getVMachines() {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//
-//            vMachines = session.createQuery("FROM VMachine ", VMachine.class).getResultList();
-//
-//            transaction.commit();
-//            return vMachines;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-        return null;
+        throw new RuntimeException("Not implemented yet");
     }
 
     public VMachine getVMachineByID(long ID) {
-//        try (Session session = sessionFactory.openSession()) {
-//            Transaction transaction = session.beginTransaction();
-//
-//            VMachine vMachine = session.createQuery("FROM VMachine vm WHERE vm.vMachineID = :vMachineID", VMachine.class)
-//                    .setParameter("vMachineID", ID)
-//                    .uniqueResult();
-//
-//            transaction.commit();
-//            return vMachine;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-        return null;
+        throw new RuntimeException("Not implemented yet");
     }
 }
